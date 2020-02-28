3103261b9d81c6d28ccea474402c038360a9995b
Kannan321 <ananthu1998@gmail.com>
2020-02-28 22:52:06 +0530

    Admin Page Work Progress

---
+++ RescueMe/app/src/main/java/com/example/ananthuks/rescueme/volunteerloginregi.java
@@ -0,0 +1,13 @@
+package com.example.ananthuks.rescueme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class volunteerloginregi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_volunteerloginregi);
+    }
+}
